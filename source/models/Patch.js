52e1951361164372b75a811c5e5905ee54f47d49
Andrew McPherson <mcphandr@amazon.com>
2019-03-21 17:43:31 -0700

    Added click events to the patches.

--- source/models/Patch.js
+++ source/models/Patch.js
@@ -1,8 +1,16 @@
+import model from "models/.js"
+
+// @class Patch
+// A patch is the piece of land that
+// players can seed, water and harvest.
 export default class Patch {
     constructor() {
         // ..
     }
     update(delta) {
         // ...
+    }
+    onClick() {
+        console.log(this)
     }
 }
--- source/views/game/Patch.view.js
+++ source/views/game/Patch.view.js
@@ -1,11 +1,18 @@
 import Preact from "preact"
 
+import model from "models/.js"
+
 export default class Patch {
     render() {
         return (
-            <div class="Patch">
+            <div class="Patch" onClick={this.onClick}>
                 patch
             </div>
         )
+    }
+    get onClick() {
+        return (event) => {
+            this.props.patch.onClick()
+        }
     }
 }
